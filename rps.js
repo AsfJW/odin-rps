2ec8b050214bdc4f1d13e89437426a2168a3ddea
Audrey Williams <audreysfwilliams@gmail.com>
2022-02-21 12:56:13 +0000

    score boxes

--- rps.js
+++ rps.js
@@ -28,33 +28,76 @@ function playRound(playerSelection, computerSelection) {
 	else if (playerSelection.toLowerCase() === computerSelection) {return 'It\'s a tie!';}
 }
 
+const pScore = document.querySelector('#playerScore');
+const cScore = document.querySelector('#computerScore');
 
-function game() {	
-		let playerSelection = prompt('What\'s your choice?');
-		const computerSelection = computerPlay();
-		console.log('Computer says: ' + computerSelection);
-		console.log('You picked: ' + playerSelection);	
-		console.log(playRound(playerSelection, computerSelection));
-		if (playRound(playerSelection, computerSelection) === 'You win!') {
-			playerCount++
+
+const btns = document.querySelectorAll('button');
+
+btns.forEach((button) => {
+	button.addEventListener('click', () => {
+		computerSelection = computerPlay();
+		console.log(playerCount);
+		if (button.id === 'rock') {
+			playerSelection = 'rock';
+			let round = playRound(playerSelection, computerSelection);
+			if (round === 'You win!') {
+				pScore.textContent = `${playerCount++}`;
+			}
+			else if (round === 'You lose!') {
+				cScore.textContent = `${computerCount++}`;
+			}
 		}
-		else if (playRound(playerSelection, computerSelection) === 'You lose!') {
-			computerCount++
-		};
-		console.log('Score\n' + 'You: ' + playerCount + ' ' + 'Computer: ' +  computerCount);
-}
-
-for (let i = 0; i < 5; i++) {
-	game();
-	if (i == 4) {
-		if (playerCount != computerCount) {
-		let winner = playerCount > computerCount ? 'Player' : 'Computer';
-		alert('Winner is: ' + winner);
+		else if (button.id === 'paper') {
+			playerSelection = 'paper';
+			let round = playRound(playerSelection, computerSelection);
+			if (round === 'You win!') {
+				playerCount++;
+			}
+			else if (round === 'You lose!') {
+				computerCount++;
+			}
 		}
-		else alert('It\s a tie!');
-	}
+		else if (button.id === 'scissors') {
+			playerSelection = 'scissors';
+			let round = playRound(playerSelection, computerSelection);
+			if (round === 'You win!') {
+				playerCount++;
+			}
+			else if (round === 'You lose!') {
+				computerCount++;
+			}
+		}
+	})
+})
 
-}
+
+// function game() {	
+// 		let playerSelection = prompt('What\'s your choice?');
+// 		const computerSelection = computerPlay();
+// 		console.log('Computer says: ' + computerSelection);
+// 		console.log('You picked: ' + playerSelection);	
+// 		console.log(playRound(playerSelection, computerSelection));
+// 		if (playRound(playerSelection, computerSelection) === 'You win!') {
+// 			playerCount++
+// 		}
+// 		else if (playRound(playerSelection, computerSelection) === 'You lose!') {
+// 			computerCount++
+// 		};
+// 		console.log('Score\n' + 'You: ' + playerCount + ' ' + 'Computer: ' +  computerCount);
+// }
+
+// for (let i = 0; i < 5; i++) {
+// 	game();
+// 	if (i == 4) {
+// 		if (playerCount != computerCount) {
+// 		let winner = playerCount > computerCount ? 'Player' : 'Computer';
+// 		alert('Winner is: ' + winner);
+// 		}
+// 		else alert('It\s a tie!');
+// 	}
+
+// }
 
 
 
